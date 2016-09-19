87f5c4d565153aab6548f9f2b4496c63461015a3
Humakt83 <urlact@yahoo.com>
2016-09-19 22:35:29 +0300

    Fetch plants from server

--- www/js/controllers/tools-controller.js
+++ www/js/controllers/tools-controller.js
@@ -38,20 +38,22 @@ angular.module('aqu-scape').controller('ToolsController', [ '$scope', '$ionicMod
     var widthOfPlant = 22;
     $scope.width = document.getElementById('palette').clientWidth;
     var numberOfPlantsShown = Math.floor($scope.width / widthOfPlant) - 2;
-    var currentStartingIndex = 0;
+    var currentStartingIndex = 0;  
 
-    plants.forEach(function(plant) {
-        var minimumGreen = 100;
-        var green = Math.max(Math.floor(Math.random() * 255), minimumGreen);
-        var red = Math.floor(Math.random() * green);
-        var blue = Math.floor(Math.random() * green);
-        plant.color = "rgb(" + red + ", " + green + ", " + blue + ")";
-        plant.style = {"background-color": "rgb(" + red + ", " + green + ", " + blue + ")"};
-    });
-
-    $scope.plants = plants;
-
-    $scope.leftArrow();
+    plants.query(function(plantArray) {
+        plantArray.forEach(function(plant) {
+            var minimumGreen = 100;
+            var green = Math.max(Math.floor(Math.random() * 255), minimumGreen);
+            var red = Math.floor(Math.random() * green);
+            var blue = Math.floor(Math.random() * green);
+            plant.color = "rgb(" + red + ", " + green + ", " + blue + ")";
+            plant.style = {"background-color": "rgb(" + red + ", " + green + ", " + blue + ")"};
+        })
+    }).$promise.then(function(result) {
+        $scope.plants = result;
+        $scope.leftArrow();  
+    });    
+    
     
     $scope.newCanvas = function() {
         $scope.modal.show();
--- www/js/services/plant-service.js
+++ www/js/services/plant-service.js
@@ -1,44 +1,5 @@
-angular.module('aqu-scape').factory('PlantService', [ function() {
+angular.module('aqu-scape').factory('PlantService', ['$resource', function($resource) {
 
-    function Plant(scientificName, finnishName, diameter) {
-        this.scientificName = scientificName;
-        this.finnishName = finnishName;
-        this.diameter = diameter;
-    }
-    
-    var plantsArray = [new Plant('Crinum calamistratum', 'Kiharakriinumi', 15),
-        new Plant('Ceratophyllum submersum', 'Hentokarvalehti', 25),
-        new Plant('Taxiphyllum barbieri', 'Jaavansammal', 20),
-        new Plant('Taxiphyllum barbieri', 'Kelluhankasammal', 10),
-        new Plant('Microsorum pteropus', 'Jaavansaniainen', 5),
-        new Plant('Ceratopteris thalictroides', 'Vesisaniainen', 20),
-        new Plant('Vallisneria spiralis', 'Vallisneria', 18),
-        new Plant('Rotala rotundifolla', 'Hentorotala', 15),
-        new Plant('Pistia stratiotes', 'Pistia', 22),
-        new Plant('Nymphaea micrantha', 'Rusolumme', 27),
-        new Plant('Najas guadalupensis', 'Hentonäkinruoho', 29),
-        new Plant('Mayaca fluviatilis', 'Hapsuruoho', 30),
-        new Plant('Myriophyllum tuberculatum', 'Ruosteärviä', 32),
-        new Plant('Limnophila sessiliflora', 'Vedensuosikki', 33),
-        new Plant('Hygrophila polysperma', 'Intianvesitähdikki', 13),
-        new Plant('Hydrocotyle leucocephala', 'Konnanputki', 10),
-        new Plant('Elodea canadensis', 'Kanadanvesirutto', 8),
-        new Plant('Echidonorus `Rubin`', 'Rubiinivesimiekka', 7),
-        new Plant('Echidonorus `Red Special`', 'Ruostevesimiekka', 4),
-        new Plant('Echidonorus grisebachii', 'Amazoninvesimiekka', 2),
-        new Plant('Anubias barteri var. barteri', 'Herttakeihäslehti', 5),
-        new Plant('Anubias barteri var. coffeifolia', 'Kahvikeihäslehti', 10),
-        new Plant('Saururus cernuus', 'Herttalehti', 20),
-        new Plant('Gymnocoronis spilanthoides', 'Vesiasteri', 11),
-        new Plant('Microsorum pteropus', 'Jaavansaniainen', 22),
-        new Plant('Cryptocorune xwillisii', 'Kääpiömelalehti', 18),
-        new Plant('Cryptocorune undulata', 'Aaltomelalehti', 19),
-        new Plant('Eleocharis acicularis', 'Hapsiluikka', 5)
-    ];
+    return $resource('http://localhost:8888/plants/lite');
 
-    return {
-        getPlants : function() {
-            return plantsArray;
-        }
-    }
 }]);
