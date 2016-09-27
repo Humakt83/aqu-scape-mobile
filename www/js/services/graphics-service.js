59c5afe2a7ba6b0bf712e57a076eae4966d672df
Humakt83 <urlact@yahoo.com>
2016-09-27 22:59:45 +0300

    Make plant appear at the center of pointer

--- www/js/services/graphics-service.js
+++ www/js/services/graphics-service.js
@@ -14,7 +14,10 @@ angular.module('aqu-scape').factory('GraphicsService', ['AquariumDimensions', fu
 
     return {
         drawEllipseFittingCanvasAndDimension: function(point, diameter, color) {
-            var rectangle = new paper.Rectangle(point, new paper.Size(scaleDepthToFittingDimensions(diameter), scaleWidthToFittingDimensions(diameter)));
+            var size = new paper.Size(scaleDepthToFittingDimensions(diameter), scaleWidthToFittingDimensions(diameter))
+            point.x = point.x - (size.width / 2);
+            point.y = point.y - (size.height / 2);
+            var rectangle = new paper.Rectangle(point, size);
             var ellipse = new paper.Path.Ellipse(rectangle);
             ellipse.fillColor = color;
             return ellipse;
